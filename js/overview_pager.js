a42ba443edf7aaff61e5560fac51c1fcdb681e5e
Martin Giessing <mgp@novicell.dk>
2022-12-08 10:48:20 +0100

    Fixed conflict with facets and the pagination hidden submit

--- js/overview_pager.js
+++ js/overview_pager.js
@@ -1,5 +1,12 @@
 Drupal.behaviors.overview_pager = {
   attach: function (context, settings) {
+    jQuery('.overview-form input', context).keydown(function(e) {
+      if (e.keyCode == 13) {
+        e.stopPropagation();
+        e.preventDefault();
+        jQuery(e.target).blur();
+      }
+    });
     jQuery('.overview-form-contents .pager .pager__item a', context).click(function(e) {
       e.preventDefault();
       let searchParams = new URLSearchParams(jQuery(this).attr('href'));
