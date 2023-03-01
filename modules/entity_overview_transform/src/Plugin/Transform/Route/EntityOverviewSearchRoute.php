aeb4fc37ec723b5a5adcd42ffd9aab6fb1d24ed6
Martin Giessing <mgp@novicell.dk>
2023-03-01 12:37:11 +0100

    Added search route transform to Entity Overview Search

---
+++ modules/entity_overview_transform/src/Plugin/Transform/Route/EntityOverviewSearchRoute.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\entity_overview_transform\Plugin\Transform\Route;
+
+
+use Drupal\Core\Url;
+use Drupal\entity_overview\OverviewFilter;
+use Drupal\entity_overview_transform\Transform\OverviewResultTransform;
+use Drupal\transform_api\RouteTransformBase;
+use Drupal\transform_api\Transform\TransformInterface;
+
+/**
+ * @RouteTransform(
+ *  id = "entity_overview_search.search",
+ *  title = "Search"
+ * )
+ */
+class EntityOverviewSearchRoute extends RouteTransformBase {
+
+  public function transform(TransformInterface $transform): array {
+    $config = \Drupal::config('entity_overview_search.settings');
+    $overview_id = $config->get('overview') ?? NULL;
+    if (empty($overview_id)) {
+      return [];
+    }
+    $filter = new OverviewFilter($overview_id, $config->get('filter') ?? []);
+    $overview = $filter->getOverview();
+    $filter->setPagination(TRUE);
+    $filter->setShowTotal($overview->getShowTotal());
+
+    $endpoint = Url::fromRoute('entity_overview_transform.overview_result.transform_mode', ['overview' => $overview_id, 'transform_mode' => $filter->getViewMode()]);
+    $transformation = [
+      'type' => 'overview_form',
+      'overview' => $overview_id,
+      'endpoint' => $endpoint->toString(),
+      'initial' => $filter->toArray(),
+      'facets' => []
+    ];
+    /** @var \Drupal\entity_overview\OverviewManager $overviewManager */
+    $overviewManager = \Drupal::service('entity_overview.manager');
+    foreach ($overviewManager->getAllFieldInfos($filter->getOverview()) as $field => $info) {
+      if ($filter->hasFacet($field)) {
+        $transformation['facets'][$field] = $info->getFieldFormTransform($filter);
+      }
+    }
+    $transformation['content'] = new OverviewResultTransform($filter);
+
+    return $transformation;
+  }
+
+}
